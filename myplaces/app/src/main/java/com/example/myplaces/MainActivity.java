149ec12672f210f1e6f731bcd6e38963c4443de3
Puakii <78651465+Puakii@users.noreply.github.com>
2024-12-13 23:20:13 +0800

    add more buttons

--- myplaces/app/src/main/java/com/example/myplaces/MainActivity.java
+++ myplaces/app/src/main/java/com/example/myplaces/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,59 +19,52 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button conection;
+
     Socket s;
     PrintWriter writer;
-    TextView number;
-    int i = 0;
-    String mens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        conection = (Button) findViewById(R.id.conection);
-        number = (TextView) findViewById(R.id.number);
-
-        onBtnClick();
-    }
-
-    public void onBtnClick() {
-
-        conection.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                BackGroundTask b1 = new BackGroundTask();
-                b1.execute();
+        LinearLayout buttonColumn = findViewById(R.id.button_column);
+        for (int i = 0; i < buttonColumn.getChildCount(); i++) {
+            View child = buttonColumn.getChildAt(i);
+            if (child instanceof Button) {
+                child.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        String tag = (String) view.getTag();
+                        BackGroundTask b1 = new BackGroundTask(tag);
+                        b1.execute();
+                    }
+                });
             }
-        });
+        }
+
+
     }
 
     class BackGroundTask extends AsyncTask<String, Void, Void> {
 
-        Handler h = new Handler();
+        private String message;
+        public BackGroundTask(String message) {
+            this.message = message;
+        }
+
         @Override
         protected Void doInBackground(String... voids) {
             try {
-                mens = String.valueOf(i);
-
                 if(s == null){
                     //change it to your IP
-                    s = new Socket("172.16.42.25",6000);
+                    s = new Socket("0.0.0.0",6000);
                     writer = new PrintWriter(s.getOutputStream());
                     Log.i("i", "CONNECTED");
                 }
-                writer.write(mens);
+                writer.write(message);
                 writer.flush();
                 //writer.close();
-                i = i+1;
-                h.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        number.setText(mens);
-                    }
-                });
             }
             catch (IOException e) {
                 e.printStackTrace();
